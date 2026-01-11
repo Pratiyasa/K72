5cceee73318a4a7fb0c07f54589c3e8c6be48732
Pratiyasa <pratyasha.2020.18@gmail.com>
2026-01-11 08:41:14 +0530

    initial page reload animation

--- src/Components/home/Video.jsx
+++ src/Components/home/Video.jsx
@@ -4,7 +4,7 @@ const video = () => {
   return (
     <div>
         <div className="h-full w-full">
-            <video className="h-full w-full" autoPlay muted loop src="https://player.vimeo.com/progressive_redirect/playback/1119600858/rendition/1080p/file.mp4?loc=external&log_user=0&signature=c4a137161d6ce80a52c50c7ee23d4fdf8df103bfc816252fc304e317a43bacc6"></video>
+            <video className="h-full w-full" autoPlay muted loop src="../../../public/Video/69496b2d.mp4"></video>
         </div>
     </div>
   )
